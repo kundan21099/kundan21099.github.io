cbfba82ebcee1e476f474e5092d749d9b5bdce0d
kd1099 <kundangaikwad21099@gmail.com>
2025-12-12 20:53:33 +0530

    To execute animation of slider: renamed js file to not use the default on gihub pages added respective path of js in default.html

--- assets/js/skills.js
+++ assets/js/skills.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("skills.js is loaded!");
   const skillBars = document.querySelectorAll('.skill-level');
   skillBars.forEach(bar => {
     const level = bar.getAttribute('data-level');
